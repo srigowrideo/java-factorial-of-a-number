8f142de6959d585434a4fa412d9b6156c64fc1b0
Srigowri Deogirikar <32318383+srigowrideo@users.noreply.github.com>
2021-05-07 16:11:15 +0530

    Add files via upload
    To find the factorial of a number in java using two types of codes.

---
+++ factoralloop.java
@@ -0,0 +1,18 @@
+package fibonacciseries;
+
+public class factoralloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+			  int i,fact=1;  
+			  int number=5;//It is the number to calculate factorial    
+			  for(i=1;i<=number;i++){    
+			      fact=fact*i;    
+			  }    
+			  System.out.println("Factorial of "+number+" is: "+fact);    
+			 }  
+			  
+	}
+
+
